447c8900e65dcdfa6acf7cc82afbd06723ec2e68
Paul12pp <30394467+Paul12pp@users.noreply.github.com>
2021-05-07 13:11:39 -0400

    changes

---
+++ Mastermind.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NobodyCare
+{
+
+    public class Mastermind
+    {
+        private readonly List<CodePeg> code;
+        private HashSet<Movement> movements;
+        private HashSet<CodePeg> pegs;
+
+        public Mastermind(List<CodePeg> code)
+        {
+            this.code = code;
+            movements = code.Select((color, order) => new Movement { Order = order, Color = color }).ToHashSet();
+            pegs = code.ToHashSet();
+        }
+
+        public List<ResultPeg> GetHints(List<CodePeg> guess)
+        {
+            var whites = new List<ResultPeg>();
+            var blacks = new List<ResultPeg>();
+
+            for (int i = 0; i < guess.Count; i++)
+            {
+                var color = guess[i];
+                var movement = new Movement { Order = i, Color = color };
+                if (movements.Contains(movement))
+                {
+                    blacks.Add(ResultPeg.Black);
+                }
+                else if (pegs.Contains(color))
+                {
+                    whites.Add(ResultPeg.White);
+                }
+            }
+            return blacks.Concat(whites).Take(4).ToList();
+        }
+    }
+
+    public enum CodePeg
+    {
+        Black,
+        Blue,
+        Green,
+        Red,
+        White,
+        Yellow,
+    }
+
+    public enum ResultPeg
+    {
+        Black,
+        White,
+    }
+
+}
---
+++ Movement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NobodyCare
+{
+    public class Movement : IEquatable<Movement>
+    {
+        public int Order { get; set; }
+        public CodePeg Color { get; set; }
+
+        public bool Equals(Movement move)
+        {
+            return move != null && Order == move.Order && Color == move.Color;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            return Equals(obj as Movement);
+        }
+
+        public override int GetHashCode()
+        {
+            return Order.GetHashCode() ^ Color.GetHashCode();
+        }
+    }
+}
